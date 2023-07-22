a2dfc9f09db4ec9d168cd13b669cccf97b7438fc
Silas <silasfas@hotmail.com>
2023-07-22 01:20:20 -0300

    js html and css update

--- index.js
+++ index.js
@@ -5,13 +5,18 @@ const year = window.document.querySelector('#year')
 
 const button = window.document.querySelector('#button')
 
-// validation varibles
+// validation variables
 const dayRequired = window.document.getElementById('dayRequired')
 const monthRequired = window.document.getElementById('monthRequired')
 const yearRequired = window.document.getElementById('yearRequired')
+
 const dayLabel = window.document.getElementById('dayLabel')
 const monthLabel = window.document.getElementById('monthLabel')
 const yearLabel = window.document.getElementById('yearLabel')
+
+const valid_day = window.document.getElementById('valid_day')
+const valid_month = window.document.getElementById('valid_month')
+const valid_year = window.document.getElementById('valid_year')
 
 // Result
 const yearsResult = window.document.getElementById('yearsResult')
@@ -19,19 +24,36 @@ const monthsResult = window.document.getElementById('monthsResult')
 const daysResult = window.document.getElementById('daysResult')
 
 button.addEventListener('click', (() => {
+    const currentDate = new Date()
+    let currentYear = currentDate.getFullYear()
 
     if (day.value == '') {
         dayRequired.style.display = 'block'
         day.style.borderColor = 'rgba(255, 0, 0, 0.411)'
         dayLabel.style.color = 'rgba(134, 1, 1, 0.562)'
-
-    } else if (month.value == '') {
+    }
+    else if (day.value < 1 || day.value > 31) {
+        valid_day.style.display = 'block'
+        day.style.borderColor = 'rgba(255, 0, 0, 0.411)'
+        dayLabel.style.color = 'rgba(134, 1, 1, 0.562)'
+    }
+    else if (month.value == '') {
         monthRequired.style.display = 'block'
         month.style.borderColor = 'rgba(255, 0, 0, 0.411)'
         monthLabel.style.color = 'rgba(134, 1, 1, 0.562)'
-
-    } else if (year.value == '') {
+    }
+    else if (month.value < 1 || month.value > 12) {
+        valid_month.style.display = 'block'
+        month.style.borderColor = 'rgba(255, 0, 0, 0.411)'
+        monthLabel.style.color = 'rgba(134, 1, 1, 0.562)'
+    }
+    else if (year.value == '') {
         yearRequired.style.display = 'block'
+        year.style.borderColor = 'rgba(255, 0, 0, 0.411)'
+        yearLabel.style.color = 'rgba(134, 1, 1, 0.562)'
+    }
+    else if (year.value > currentYear) {
+        valid_year.style.display = 'block'
         year.style.borderColor = 'rgba(255, 0, 0, 0.411)'
         yearLabel.style.color = 'rgba(134, 1, 1, 0.562)'
     }
@@ -40,23 +62,9 @@ button.addEventListener('click', (() => {
         let monthNumber = Number(month.value)
         let yearNumber = Number(year.value)
 
-        const currentDatedate = new Date()
         let userBirthDate = new Date(yearNumber, monthNumber - 1, dayNumber);
 
-        // console.log(userBirthDate)
-        // console.log(userBirthDate.getTime())
-
-        // if (
-        //     isNaN(userBirthDate.getTime()) || // Check if the userBirthDate is valid
-        //     dayValue <= 0 || dayValue > 31 || // Validate day
-        //     monthValue <= 0 || monthValue > 12 || // Validate month
-        //     yearValue <= 0 // Validate year
-        // ) {
-        //     console.error('Invalid date entered.');
-        //     return;
-        // }
-
-        let ageInMilliseconds = currentDatedate - userBirthDate
+        let ageInMilliseconds = currentDate - userBirthDate
         const ageDate = new Date(ageInMilliseconds)
 
         const years = ageDate.getUTCFullYear() - 1970
